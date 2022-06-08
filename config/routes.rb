d024042664c9e98b86eb917ddb5de69d621990df
Alex-JX <alexjx17@gmail.com>
2022-06-07 17:19:25 -0700

    Edit messages with Turbo Stream

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root "messages#index"
-  resources :messages
+  resources :messages do 
+    member do 
+      # I need acces to Edit by Post 'cause of turbo_stream doens't proccess GET
+      post :edit
+    end
+  end
 end
