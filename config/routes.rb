64aac33df4be28de84574dfa2abd2ff845569a1e
Alex-JX <alexjx17@gmail.com>
2022-06-10 13:02:00 -0700

    changed the index style

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "messages#index"
   resources :messages do 
     member do 
-      # It's necesary the acess to Edit by POST 'cause of turbo_stream doesn't proccess GET 
+       # It's necesary the acess to Edit by POST because of turbo_stream doesn't proccess GET 
       post :edit
     end
   end
